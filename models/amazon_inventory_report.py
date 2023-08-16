25238572ea5d639d2575c2e41e9a0b8e7612aec5
Saqlain Tahir <saqlain.tahir@qbatch.com>
2023-08-16 17:30:55 +0500

    Fix: Update Model Of Amazon Inventory Report

--- models/amazon_inventory_report.py
+++ models/amazon_inventory_report.py
@@ -3,6 +3,7 @@ from ops.helpers.db_config import db_conn
 
 Model.set_connection_resolver(db_conn())
 
+
 class AmazonInventoryReport(Model):
     __table__ = "amazon_inventory_report"
     __fillable__ = [
@@ -11,6 +12,18 @@ class AmazonInventoryReport(Model):
         "price",
         "quantity",
         "marketplace_id",
+        "Business Price",
+        "Quantity Price Type",
+        "Quantity Lower Bound 1",
+        "Quantity Price 1",
+        "Quantity Lower Bound 2",
+        "Quantity Price 2",
+        "Quantity Lower Bound 3",
+        "Quantity Price 3",
+        "Quantity Lower Bound 4",
+        "Quantity Price 4",
+        "Quantity Lower Bound 5",
+        "Quantity Price 5",
         "created_at",
         "updated_at",
     ]
