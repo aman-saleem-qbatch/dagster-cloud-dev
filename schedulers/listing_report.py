0fa4b68ac234a6f79ec1c90d30a230ad268c3911
Saqlain Tahir <saqlain.tahir@qbatch.com>
2023-08-21 14:10:35 +0500

    Fix: Change Job Name In Scheduler

--- schedulers/listing_report.py
+++ schedulers/listing_report.py
@@ -4,6 +4,6 @@ from dagster import ScheduleDefinition
 def listing_report():
     return ScheduleDefinition(
         cron_schedule="0 */12 * * *",  # every 12 hours
-        job_name="create_report_job",
+        job_name="amz_request_listing_report_job",
         execution_timezone="US/Central",
     )
